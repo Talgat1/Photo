14c1b8a57cd14531990decdb5d77d00206dfa839
arullintalgat36@gmail.com <arullintalgat36@gmail.com>
2022-02-13 00:33:44 +0300

    Add galery and camera

--- Photo/Photo/App.xaml.cs
+++ Photo/Photo/App.xaml.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Photo.DB;
+using System.IO;
 
 namespace Photo
 {
     public partial class App : Application
     {
+        public const string DB_NAME = "clientsProj.db";
+        public static CRUDOperation db;
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        public static CRUDOperation Db
+        {
+            get
+            {
+                if (db == null)
+                {
+                    db = new CRUDOperation(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DB_NAME));
+                }
+                return db;
+            }
         }
 
         protected override void OnStart()
